56fe8416b7dee224a3c448efab07e6910cd620e1
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:43 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 16,
+  "doc_id": "frus1989-92v14",
+  "title": "1989\u20131992, Volume XIV, Arab-Israeli Dispute",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v14",
+  "repo_slug": "frus-1989-92v14-assist",
+  "title_ascii": "1989-1992, Volume XIV, Arab-Israeli Dispute",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v14-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v14-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XIV+OR+Arab-Israeli+OR+Dispute&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
